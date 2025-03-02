1c5b0db76723f644143bb387da99b9ffeffc6d47
Zia <zia_gt@yahoo.com>
2025-03-02 23:03:31 +0100

    Drum machine

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import DrumMachine from "./components/DrumMachine"; // No need to change the import
+
+function App() {
+  return (
+    <div className="App">
+      <DrumMachine />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DrumMachine.tsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+
+const drumPads = [
+  { key: "Q", sound: "Heater 1", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" },
+  { key: "W", sound: "Heater 2", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" },
+  { key: "E", sound: "Heater 3", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" },
+  { key: "A", sound: "Heater 4", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4.mp3" },
+  { key: "S", sound: "Clap", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" },
+  { key: "D", sound: "Open-HH", src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" },
+  { key: "Z", sound: "Kick-n'-Hat", src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" },
+  { key: "X", sound: "Kick", src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" },
+  { key: "C", sound: "Closed-HH", src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" },
+];
+
+const DrumMachine = () => {
+  const [display, setDisplay] = useState("Press a key");
+
+  const playSound = (key, sound) => {
+    const audio = document.getElementById(key);
+    if (audio) {
+      audio.currentTime = 0;
+      audio.play();
+      setDisplay(sound);
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    const pad = drumPads.find((pad) => pad.key === event.key.toUpperCase());
+    if (pad) {
+      playSound(pad.key, pad.sound);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyPress);
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
+
+  return (
+    <div id="drum-machine" className="container">
+      <div id="display" className="display">{display}</div>
+      <div className="drum-pads">
+        {drumPads.map((pad) => (
+          <button
+            key={pad.key}
+            className="drum-pad"
+            id={pad.sound}
+            onClick={() => playSound(pad.key, pad.sound)}
+          >
+            {pad.key}
+            <audio className="clip" id={pad.key} src={pad.src}></audio>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DrumMachine;
